15486b5abb9c93b5269d8a14d078dfced6f91823
Rendi Kasigi <rkasigi@gmail.com>
2019-05-20 21:53:29 +0700

    update compile

--- bin/util/oci8.sh
+++ bin/util/oci8.sh
@@ -4,6 +4,11 @@ install_oci8_ext() {
 	# special treatment for oci8;
 	# otherwise users would have to have it in their require section, which is annoying in development environments
 
+	engineoci="unknown"
+	if composer show -d "$build_dir/.heroku/php" --installed --quiet heroku-sys/php 2>/dev/null; then
+    	engineoci="php"
+    fi
+
 	if [[ "$engine" == "php" ]] && ! $engine -n $(which composer) show -d "$build_dir/.heroku/php" --installed --quiet heroku-sys/ext-oci8 2>/dev/null; then
 		echo /app/.heroku/php/lib/instantclient_11_2 > /etc/ld.so.conf.d/oracle-instantclient.conf
 		ldconfig
