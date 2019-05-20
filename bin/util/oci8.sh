0ba0723d240aacb6e31ca98ea8f16670dc9c7afd
Rendi Kasigi <rkasigi@gmail.com>
2019-05-20 21:55:24 +0700

    update compile

--- bin/util/oci8.sh
+++ bin/util/oci8.sh
@@ -9,7 +9,7 @@ install_oci8_ext() {
     	engineoci="php"
     fi
 
-	if [[ "$engine" == "php" ]] && ! $engine -n $(which composer) show -d "$build_dir/.heroku/php" --installed --quiet heroku-sys/ext-oci8 2>/dev/null; then
+	if [[ "$engineoci" == "php" ]] && ! $engineoci -n $(which composer) show -d "$build_dir/.heroku/php" --installed --quiet heroku-sys/ext-oci8 2>/dev/null; then
 		echo /app/.heroku/php/lib/instantclient_11_2 > /etc/ld.so.conf.d/oracle-instantclient.conf
 		ldconfig
 	fi
