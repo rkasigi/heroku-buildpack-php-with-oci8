0443a2f8f11f65d554dd25dc7797d495d0433895
Rendi Kasigi <rkasigi@gmail.com>
2019-05-20 22:21:32 +0700

    update compile

--- bin/util/oci8.sh
+++ bin/util/oci8.sh
@@ -5,10 +5,11 @@ install_oci8_ext() {
 	# otherwise users would have to have it in their require section, which is annoying in development environments
 
     status "start process install_oci8_ext"
+    status whoami
 
 	if [[ "$engine" == "php" ]] && ! $engine -n $(which composer) show -d "$build_dir/.heroku/php" --installed --quiet heroku-sys/ext-oci8 2>/dev/null; then
 	    status "installing instaclient"
-		echo /app/.heroku/php/lib/instantclient_11_2 > /etc/ld.so.conf.d/oracle-instantclient.conf
+		sh -c "echo '/app/.heroku/php/lib/instantclient_11_2' > /etc/ld.so.conf.d/oracle-instantclient.conf"
 		ldconfig
 	fi
 
