34596968a0d349855f73dc7b37c8faf102898941
Rendi Kasigi <rkasigi@gmail.com>
2019-05-23 23:08:42 +0700

    cleanup oci8

--- bin/util/oci8.sh
+++ bin/util/oci8.sh
@@ -4,8 +4,7 @@ install_oci8_ext() {
 	# special treatment for oci8;
 	# otherwise users would have to have it in their require section, which is annoying in development environments
 
-    status "start process install_oci8_ext"
-    status `whoami`
+    status "Starting process install_oci8_ext"
 
 	if [[ "$engine" == "php" ]] && ! $engine -n $(which composer) show -d "$build_dir/.heroku/php" --installed --quiet heroku-sys/ext-oci8 2>/dev/null; then
 	    status "installing instaclient"
@@ -17,7 +16,6 @@ install_oci8_ext() {
 		echo "if [ -z ${LD_LIBRARY_PATH+x} ]; then export LD_LIBRARY_PATH=/app/.heroku/php/lib/instantclient_11_2; else export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/app/.heroku/php/lib/instantclient_11_2; fi" > $build_dir/.profile.d/00-oci8-env.sh
 	fi
 
-	status $engine
-	status "done process install_oci8_ext"
+	status "Finished install_oci8_ext"
 
 }
