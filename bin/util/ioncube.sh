e9d24c8454161fd0722eff2aa02c7894add32dcf
Rendi Kasigi <rkasigi@gmail.com>
2019-09-04 11:07:32 +0700

    update ioncube

--- bin/util/ioncube.sh
+++ bin/util/ioncube.sh
@@ -4,7 +4,7 @@ install_ioncube_ext() {
 
     status "Starting process install_ioncube_ext"
 
-    curl_retry_on_18 --fail --silent --location -o $build_dir/.heroku/php/ioncube.tar.gz "https://github.com/rkasigi/heroku-buildpack-php-repository/blob/master/dist-heroku-18-stable/extensions/no-debug-non-zts-20180731/ioncube.tar.gz" || {
+    curl_retry_on_18 --fail --silent --location -o $build_dir/.heroku/php/ioncube.tar.gz "https://github.com/rkasigi/heroku-buildpack-php-repository/raw/master/dist-heroku-18-stable/extensions/no-debug-non-zts-20180731/ioncube.tar.gz" || {
     	mcount "failures.bootstrap.download.ioncube"
     	error <<-EOF
     		Failed to download ioncube extension!
